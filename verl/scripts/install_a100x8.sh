829da444da503a5f8ebfc1f63cac5d16106423ec
Wang Siyuan <v-siywang@microsoft.com>
2025-03-04 17:21:43 +0000

    fix installation script

--- verl/scripts/install_a100x8.sh
+++ verl/scripts/install_a100x8.sh
@@ -1,26 +1,37 @@
+#!/bin/bash
+
+# Record the current directory
+CURRENT_DIR=$(pwd)
+
+# Set up conda environment
 cd ~
 source /opt/conda/etc/profile.d/conda.sh 
 conda create -y -n rstar python==3.12
 conda activate rstar
 
-# tensordict
+# Install tensordict
 cd ~
 git clone https://github.com/pytorch/tensordict.git
 cd tensordict
 pip install .
 cd ..
 
-# vllm
+# Install vllm and flash-attn
 pip install vllm
 pip install flash-attn
 
-# rStar-RL
-git clone https://github.com/rStar-RL/rStar-RL.git
-cd rStar-RL/verl
+# Clone and install rStar-RL
+# git clone https://github.com/rStar-RL/rStar-RL.git
+# cd rStar-RL/verl
+cd "$CURRENT_DIR"
 pip install -e .
 cd ..
 
+# Install pandas and wandb
 pip install pandas
 pip install wandb
 
+# Apply ray patch
 bash patches/ray_patch/patch.sh
+
+# Return to the original directory
