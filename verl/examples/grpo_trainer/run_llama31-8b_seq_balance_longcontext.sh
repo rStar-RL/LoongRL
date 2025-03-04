5bd7698b98566d22bf00f1287bdef208165c124c
Wang Siyuan <v-siywang@microsoft.com>
2025-03-04 07:14:58 +0000

    add llama3 longcontext

---
+++ verl/examples/grpo_trainer/run_llama31-8b_seq_balance_longcontext.sh
@@ -0,0 +1,50 @@
+set -x
+
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+# train_files="['~/r1_130k/train.parquet']"
+# test_files="['~/r1_130k/test.parquet']"
+# model_path="/mnt/longcontext/models/siyuan/llama3/"
+# train_files="['/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/musique10000_seq8192/train.parquet']"
+# test_files="['/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/musique10000_seq8192/test.parquet']"
+train_files="['/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/merged_data_deepscaler_openr1_130k_5000/train.parquet', '/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/musique5000_seq8192/train.parquet', '/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/hotpotqa5000_seq8192/train.parquet']"
+test_files="['/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/merged_data_deepscaler_openr1_130k_5000/test.parquet', '/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/musique5000_seq8192/test.parquet', '/mnt/longcontext/models/siyuan/rl_datasets/rl_three/no_system/hotpotqa5000_seq8192/test.parquet']"
+
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files="$train_files" \
+    data.val_files="$test_files" \
+    data.train_batch_size=1024 \
+    data.val_batch_size=1312 \
+    data.max_prompt_length=8192 \
+    data.max_response_length=1024 \
+    actor_rollout_ref.model.path=/mnt/longcontext/models/siyuan/llama3/Llama-3.1-8B-Instruct \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.use_dynamic_bsz=True \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.grad_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name='verl_grpo_rej_test' \
+    trainer.experiment_name='llama3_datamix555' \
+    ++trainer.val_before_train=False \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=5 \
+    trainer.test_freq=5 \
+    trainer.reward_rejection_sampling=True \
+    trainer.total_epochs=1 $@
