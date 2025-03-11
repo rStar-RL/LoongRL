4ce5ce48e5305bea090544221c245505e1955f0a
Wang Siyuan <v-siywang@microsoft.com>
2025-03-11 12:16:00 -0700

    add force reinit to wanbd tracking

--- verl/verl/utils/tracking.py
+++ verl/verl/utils/tracking.py
@@ -38,7 +38,8 @@ class Tracking(object):
 
         if 'tracking' in default_backend or 'wandb' in default_backend:
             import wandb
-            wandb.init(project=project_name, name=experiment_name, config=config)
+            wandb.init(project=project_name, name=experiment_name, config=config, reinit=True)
+            # use reinit=True to avoid multiple runs in the same process
             self.logger['wandb'] = wandb
 
         if 'mlflow' in default_backend:
