2ac86e3dadf64504e09740d0df227e34ab953241
J-shang <33053116+J-shang@users.noreply.github.com>
2025-03-21 16:15:38 +0800

    support SGLang on AMD device (#20)

---
+++ patches/vllm_patch/install_v0_7_3.sh
@@ -0,0 +1,17 @@
+# this script is used to install vllm 0.7.3 with a customized requirement version that meet sglang required
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd $SCRIPT_DIR/../../..
+
+git clone https://github.com/vllm-project/vllm.git
+pip uninstall -y vllm
+
+# align with sglang
+pip install transformers==4.48.3
+
+cd vllm
+git checkout v0.7.3
+cp $SCRIPT_DIR/requirements-common.txt
+export CCACHE_DIR=/scratch/ccahe
+python setup.py develop
+cd ..
