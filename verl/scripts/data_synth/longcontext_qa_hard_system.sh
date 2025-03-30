ddac347af8f67ebd7cde88cd00f9b550089eb430
Wang Siyuan <v-siywang@microsoft.com>
2025-03-30 14:17:09 +0000

    fix system output

--- verl/scripts/data_synth/longcontext_qa_hard_system.sh
+++ verl/scripts/data_synth/longcontext_qa_hard_system.sh
@@ -1,8 +1,6 @@
-n_samples_list=(1000 3000 5000 10000)
+n_samples_list=(1000 3000 5000)
 datasets=(
-    "hotpotqa"
     "musique"
-    "2wikimqa"
 )
 seq_lens=(8192 16384 32768)
 
@@ -10,7 +8,7 @@ for n_samples in "${n_samples_list[@]}"; do
     for dataset in "${datasets[@]}"; do
         for seq_len in "${seq_lens[@]}"; do
             start_index=0
-            total_samples=10000  # This should be the total number of samples available, adjust as needed
+            total_samples=5000  # This should be the total number of samples available, adjust as needed
 
             # Loop over start_index with a stride equal to n_samples
             while [ $start_index -lt $total_samples ]; do
@@ -21,9 +19,10 @@ for n_samples in "${n_samples_list[@]}"; do
                 fi
 
                 echo "Generating dataset: $dataset with sequence length: $seq_len, start_index: $start_index, end_index: $end_index"
+                # /mnt/longcontext/models/siyuan/test_code/longcontext_syth/musique/hard-llama-3.1-8B-instruct-num_sample_5000-max_seq_8192.jsonl 
                 python examples/data_preprocess/longcontextqa_like_dataset_system.py \
-                    --data_source /mnt/longcontext/models/siyuan/test_code/longcontext_syth/$dataset/validation-llama-3.1-8B-instruct-num_sample_10000-max_seq_$seq_len.jsonl \
-                    --local_dir /mnt/longcontext/models/siyuan/rl_datasets/rl_three/system/${dataset}_start_idx${start_index}_end_idx${end_index}_seq${seq_len} \
+                    --data_source /mnt/longcontext/models/siyuan/test_code/longcontext_syth/$dataset/hard-llama-3.1-8B-instruct-num_sample_5000-max_seq_$seq_len.jsonl \
+                    --local_dir /mnt/longcontext/models/siyuan/rl_datasets/rl_three/system/hard_${dataset}_start_idx${start_index}_end_idx${end_index}_seq${seq_len} \
                     --start_index $start_index \
                     --end_index $end_index
 
