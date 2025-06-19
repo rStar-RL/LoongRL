d554642413c7498d25918472763b848f4bef65c8
Wang Siyuan <v-siywang@microsoft.com>
2025-06-19 11:30:40 +0000

    add potential break multiprocessing compute score for naive reward managger

--- verl/verl/workers/reward_manager/naive.py
+++ verl/verl/workers/reward_manager/naive.py
@@ -16,6 +16,8 @@ from verl import DataProto
 from verl.utils.reward_score import _default_compute_score
 import torch
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 class NaiveRewardManager:
     """The reward manager.
@@ -65,24 +67,21 @@ class NaiveRewardManager:
         data.batch['acc'] = torch.tensor(scores, dtype=torch.float32, device=prompt_ids.device)
         return scores
 
-    def __call__(self, data: DataProto):
-        """We will expand this function gradually based on the available datasets"""
-
-        # If there is rm score, we directly return rm score. Otherwise, we compute via rm_score_fn
+    def __call__(self, data: DataProto, max_workers: int = 16):
+        """Multi-threaded scoring of data items."""
         if 'rm_scores' in data.batch.keys():
             return data.batch['rm_scores']
 
         reward_tensor = torch.zeros_like(data.batch['responses'], dtype=torch.float32)
 
+        # Track how many examples printed per source
         already_print_data_sources = {}
 
-        for i in range(len(data)):
-            data_item = data[i]  # DataProtoItem
+        def process_single(i):
+            data_item = data[i]
 
             prompt_ids = data_item.batch['prompts']
-
             prompt_length = prompt_ids.shape[-1]
-
             valid_prompt_length = data_item.batch['attention_mask'][:prompt_length].sum()
             valid_prompt_ids = prompt_ids[-valid_prompt_length:]
 
@@ -90,18 +89,12 @@ class NaiveRewardManager:
             valid_response_length = data_item.batch['attention_mask'][prompt_length:].sum()
             valid_response_ids = response_ids[:valid_response_length]
 
-            # decode
             prompt_str = self.tokenizer.decode(valid_prompt_ids)
             response_str = self.tokenizer.decode(valid_response_ids)
 
             ground_truth = data_item.non_tensor_batch['reward_model']['ground_truth']
-
             data_source = data_item.non_tensor_batch['data_source']
-
             extra_info = data_item.non_tensor_batch.get('extra_info', None)
-            # extra_info = data_item.non_tensor_batch.get('input', None)
-            if extra_info is not None:
-                print("extra_info", extra_info)
 
             score = self.compute_score(
                 data_source=data_source,
@@ -109,16 +102,28 @@ class NaiveRewardManager:
                 ground_truth=ground_truth,
                 extra_info=extra_info,
             )
-            reward_tensor[i, valid_response_length - 1] = score
 
-            if data_source not in already_print_data_sources:
-                already_print_data_sources[data_source] = 0
+            return i, valid_response_length.item(), score, prompt_str, response_str, ground_truth, data_source, extra_info
 
-            if already_print_data_sources[data_source] < self.num_examine:
-                already_print_data_sources[data_source] += 1
-                print("[prompt]", prompt_str)
-                print("[response]", response_str)
-                print("[ground_truth]", ground_truth)
-                print("[score]", score)
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = [executor.submit(process_single, i) for i in range(len(data))]
+
+            for future in as_completed(futures):
+                i, valid_resp_len, score, prompt_str, response_str, ground_truth, data_source, extra_info = future.result()
+                reward_tensor[i, valid_resp_len - 1] = score
+
+                if data_source not in already_print_data_sources:
+                    already_print_data_sources[data_source] = 0
+
+                if already_print_data_sources[data_source] < self.num_examine:
+                    already_print_data_sources[data_source] += 1
+
+                    if extra_info is not None:
+                        print("extra_info", extra_info)
+
+                    print("[prompt]", prompt_str)
+                    print("[response]", response_str)
+                    print("[ground_truth]", ground_truth)
+                    print("[score]", score)
 
         return reward_tensor
